8054ee19498394ff671e16ed388f9bf8ea177791
Dmitry Lihachev <lda@openteam.ru>
2011-12-05 11:04:51 +0700

    update to omniauth 1.0.1

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'sqlite3'
-gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
+gem 'devise'
 gem 'omniauth'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :token_authenticatable,
          :recoverable, :timeoutable, :trackable, :validatable, :rememberable
 
-  self.token_authentication_key = "access_token"
+  self.token_authentication_key = "oauth_token"
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   def self.find_for_token_authentication(conditions)
     where(["access_grants.access_token = ? AND (access_grants.access_token_expires_at IS NULL OR access_grants.access_token_expires_at > ?)", conditions[token_authentication_key], Time.now]).joins(:access_grants).select("users.*").first
   end
-  
+
   def initialize_fields
     self.status = "Active"
     self.expiration_date = 1.year.from_now
