46b0a0db897cc9b453a6467010f0cc8a8366fdf8
Dmitry Lihachev <lda@openteam.ru>
2011-12-05 15:08:35 +0700

    support omniauth 1.0 clients

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -14,18 +14,18 @@ class AuthController < ApplicationController
 
   def access_token
     application = Client.authenticate(params[:client_id], params[:client_secret])
-    
+
     if application.nil?
       render :json => {:error => "Could not find application"}
       return
     end
-    
+
     access_grant = AccessGrant.authenticate(params[:code], application.id)
     if access_grant.nil?
       render :json => {:error => "Could not authenticate access code"}
       return
     end
-    
+
     access_grant.start_expiry_period!
     render :json => {:access_token => access_grant.access_token, :refresh_token => access_grant.refresh_token, :expires_in => Devise.timeout_in.to_i}
   end
@@ -36,29 +36,28 @@ class AuthController < ApplicationController
 
   def user
     hash = {
-      :provider => 'my_strategy',
+      :provider => 'josh_id',
       :uid => current_user.id.to_s,
-      :user_info => {
-         :name => current_user.email, # change if required
+      :info => {
+        :name => current_user.email, # change if required
+        :first_name => current_user.first_name,
+        :last_name => current_user.last_name
       },
       :extra => {
         :admin => current_user.admin?,
-        :first_name => current_user.first_name,
-        :last_name => current_user.last_name
       }
     }
-
     render :json => hash.to_json
   end
 
   # Incase, we need to check timeout of the session from a different application!
   # This will be called ONLY if the user is authenticated and token is valid
-  # Extend the UserManager session 
+  # Extend the UserManager session
   def isalive
     warden.set_user(current_user, :scope => :user)
     response = { 'status' => 'ok' }
 
-    respond_to do |format| 
+    respond_to do |format|
       format.any { render :json => response.to_json }
     end
   end
