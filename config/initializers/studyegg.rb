54d050787846179b5b525672fd19e4e2ac7b8399
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-12-21 13:30:37 -0500

    new studyegg path init

--- config/initializers/studyegg.rb
+++ config/initializers/studyegg.rb
@@ -1,4 +1,9 @@
-STUDYEGG_PATH = "http://localhost:3003"
-STUDYEGG_STORE_PATH = "http://localhost:3002"
-STUDYEGG_QUESTIONS_PATH = "http://localhost:3001"
-STUDYEGG_USER_MANAGER_PATH = "http://localhost:3000"
+#STUDYEGG_PATH = "http://localhost:3003"
+#STUDYEGG_STORE_PATH = "http://localhost:3002"
+#STUDYEGG_QUESTIONS_PATH = "http://localhost:3001"
+#STUDYEGG_USER_MANAGER_PATH = "http://localhost:3000"
+
+STUDYEGG_PATH = "http://studyegg.heroku.com"
+STUDYEGG_STORE_PATH = "http://studyegg-store.heroku.com"
+STUDYEGG_QUESTIONS_PATH = "http://studyegg-qb.heroku.com"
+STUDYEGG_USER_MANAGER_PATH = "http://auth-provider.heroku.com"
