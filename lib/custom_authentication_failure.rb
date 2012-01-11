ed3b8e3704cd5146616ad3b36db31bc9f6c73ce6
Bill DeRusha <Bill.DeRusha@gmail.com>
2012-01-11 15:23:33 -0500

    redirect devise failed logins

--- lib/custom_authentication_failure.rb
+++ lib/custom_authentication_failure.rb
@@ -3,21 +3,6 @@ class CustomAuthenticationFailure < Devise::FailureApp
   protected 
   
   def redirect_url
-    referrer = @@referrer
-    puts "REFERRER! #{referrer}"
-    puts params[:redirect_uri]
-    ref = referrer.to_s
-    ref.gsub!(/http:\/\//, '')
-    if ref[-14..-1]=='zendolabs.com/'
-      sub = ref[0..-16]
-      puts sub
-      sub = '' if sub == 'www'
-    else
-      puts "ELSE"
-      sub = ''
-    end
-    
-    #login_path
-    "#{STUDYEGG_USER_MANAGER_PATH}/#{sub}"
+    login_path
   end 
 end
