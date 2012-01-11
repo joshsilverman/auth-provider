ff976c10dccfbd760982caf2b3a12942a424791d
Bill DeRusha <Bill.DeRusha@gmail.com>
2012-01-11 14:53:05 -0500

    redirect devise failed logins

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -169,4 +169,8 @@ Devise.setup do |config|
   #config.warden do |manager|
     #manager.default_strategies(:scope => :user).unshift(:custom_database_authenticatable)
   #end
+ 
+  config.warden do |manager|
+    manager.failure_app = CustomAuthenticationFailure
+  end
 end
